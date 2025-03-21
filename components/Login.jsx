e7f95dccfe2c0c03f92380a81c4ab9ba3fe44dfe
Elif Cetin <xxelifcetinxx@gmail.com>
2025-03-21 18:44:59 +0300

    add components files

---
+++ components/Login.jsx
@@ -0,0 +1,7 @@
+/* mail, şifre ve şartları kabul ediyorum alanından oluşacak. 
+
+form alanları için validasyonlar eklenecek:
+geçerli email (regex kullanılabilir),
+strong password (regex kullanılabilir)
+ve şartları kabul etmesi gerekecek.
+tüm validasyonları geçer ise login butonu aktif olacak.*/
